af52b93c5b50fa385ec1bc555b6397fa8a05a643
David <david@cappell.net>
2026-02-07 23:24:38 +0100

    fix: use correct func type for compareFunc

--- internal/util/list.go
+++ internal/util/list.go
@@ -4,7 +4,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/validate"
 )
 
-func ListIncludes[T any](arr []T, obj T, compareFunc validate.CompareFunc[T]) bool {
+func ListIncludes[T any](arr []T, obj T, compareFunc validate.MatchFunc[T]) bool {
 	for _, e := range arr {
 		if compareFunc(e, obj) {
 			return true
@@ -13,7 +13,7 @@ func ListIncludes[T any](arr []T, obj T, compareFunc validate.CompareFunc[T]) bo
 	return false
 }
 
-func ListMissing[T any](old []T, new []T, compareFunc validate.CompareFunc[T]) []T {
+func ListMissing[T any](old []T, new []T, compareFunc validate.MatchFunc[T]) []T {
 	result := make([]T, 0)
 	for _, e := range old {
 		if !ListIncludes(new, e, compareFunc) {
